6cea3bff41d75cedf02e3d2a0a1842c758b902da
Ashokgit25ai <anithaashok944@gmail.com>
2026-05-15 15:15:12 +0530

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import "./App.css"
 import Child from './components/Child'
 import Parent from './components/Parent'
 import Pizza1 from './assets/Pizza1.jpg'
@@ -16,33 +17,36 @@ const App = () => {
   const title3 = "Chicken Pizza"
   const price3 = "400/-"
 
-  // const [count,setCount] = useState(0);
+  const [count,setCount] = useState(0);
   const [show,setShow] = useState(false);
   const [showPass,setPass] = useState(false);
   
 
 
   return (
-    // <div className="container" >
-    //   <div className="card1">
-    //     <Child image = {Pizza1} name = {title1} rupees = {price1} className = "pizza1" />
-    //   </div>
-    //   <div className="card1">
-    //     <Child image = {pizza2} name = {title2} rupees = {price2} className = "pizza2"  />
-    //   </div>
-    //   <div className="card1">
-    //     <Child image = {pizza3} name = {title3} rupees = {price3} className = "pizza3"  />
-    //   </div>
-    //   <div>
-    //     <Parent count={count} counter={setCount} />
-    //   </div>
+    <div className='hero'>
+
+      <div className="container" >
+        <div className="card1">
+          <Child image = {Pizza1} name = {title1} rupees = {price1} className = "pizza1" />
+        </div>
+        <div className="card1">
+          <Child image = {pizza2} name = {title2} rupees = {price2} className = "pizza2"  />
+        </div>
+        <div className="card1">
+          <Child image = {pizza3} name = {title3} rupees = {price3} className = "pizza3"  />
+        </div>
+       <div>
+         <Parent count={count} counter={setCount} />
+       </div>
+      </div>
       
-    // </div>
-    <>
-      <ToggleMessage show = {show} set={setShow} />
-      <ToggleInput showPass = {showPass} setPass={setPass} />
-    </>
-    
+      <div>
+        <ToggleMessage show = {show} set={setShow} />
+        <ToggleInput showPass = {showPass} setPass={setPass} />
+      </div>
+
+    </div>
   )
 }
 
